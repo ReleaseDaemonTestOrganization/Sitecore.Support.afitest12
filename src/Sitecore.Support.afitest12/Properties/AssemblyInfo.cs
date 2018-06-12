e84fba57461150ab2cd347ff4e54b5b8a2899e51
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-06-12 15:41:53 +0300

    init

---
+++ src/Sitecore.Support.afitest12/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.afitest12")]
+[assembly: AssemblyProduct("Sitecore.Support.afitest12")]
+[assembly: ComVisible(false)]
